509681c0bff39eae711391fcf14d767e48315e96
Marc Rodenbusch <marcrodenbusch@gmail.com>
2024-09-10 23:30:23 +0200

    improved coding style

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrodenbu <mrodenbu@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: marcrodenbusch <marcrodenbusch@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 15:08:16 by mrodenbu          #+#    #+#             */
-/*   Updated: 2023/06/28 16:08:55 by mrodenbu         ###   ########.fr       */
+/*   Updated: 2024/09/10 23:29:51 by marcrodenbu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 	i = 0;
 	ptr = (char *)malloc(nmemb * size);
-	if (ptr == NULL)
+	if (!ptr)
 		return (NULL);
 	else
 	{
